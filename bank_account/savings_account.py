f0a9a39d3e5865e0ac92f188aee2e979b0343087
Beerdavindersingh <beerdavindersingh@rrc.com>
2025-02-16 16:19:11 -0600

    saving account class created.

---
+++ bank_account/savings_account.py
@@ -0,0 +1,45 @@
+from datetime import date
+from bank_account.bank_account import BankAccount
+ 
+class SavingAccount(BankAccount):
+   
+    SERVICE_CHARGE_PREMIUM = 2.00
+   
+    def __init__(self, account_number: int, client_number: int, balance: float, date_created: date, minimum_balance: float):
+        """
+        
+        """
+       
+        super().__init__(account_number, client_number, balance, date_created)
+       
+        try:
+            self.__minimum_balance = float(minimum_balance)
+        except:
+            self.__minimum_balance = 50.00
+            """
+            
+            """
+           
+    @property
+    def minimum_balance(self):
+        """
+        
+        """
+        return self.__minimum_balance
+   
+    def __str__(self) -> str:
+        """
+       
+        """
+        return (f"{super().__str__()}\n"
+                f"Minimum Balance: ${self.__minimum_balance:.2f} Account Type: Savings")
+                         
+    def get_service_charges(self):
+        """
+        
+        """
+        if self._BankAccount__balance >= self.__minimum_balance:
+            return self.BASE_SERVICE_CHARGE
+        else:
+            return self.BASE_SERVICE_CHARGE * self.SERVICE_CHARGE_PREMIUM
+                             
